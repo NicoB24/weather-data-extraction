09273983d92c7766b233313b6cfb46cac868b558
NicoB24 <nicolasbruno@Nicolass-MacBook-Pro.local>
2025-06-27 17:06:15 -0300

    finished

--- src/processor.py
+++ src/processor.py
@@ -2,6 +2,9 @@ import os
 from datetime import datetime
 from typing import Dict, List
 
+import matplotlib
+matplotlib.use("Agg")
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
